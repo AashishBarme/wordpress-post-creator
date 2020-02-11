804a97afcf3e7b63f782b4f75c3752df9ac4c8f7
AashishBarme <aashish.barme9@gmail.com>
2020-02-11 22:09:48 +0545

    xml file is added which is exact from the wordpress db post table

---
+++ config/database.php
@@ -0,0 +1,2 @@
+<?php
+echo "This is database config page";
--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
+$xml = simplexml_load_file('xml/td_posts.xml');
+$table = $xml->table;
 
-echo "Hello this is test";
+for ($i = 0 ; $i < count($table); $i++ )
+{
+    $column = $table[$i]->column;
+    for($j = 0; $j < count($column); $j++){
+        if ($column[$j]['name'] == 'post_title'):
+          echo 'Post Title:'. $column[$j].'<br>';
+        endif;
+        if ($column[$j]['name'] == 'post_date'):
+            echo 'Post Date:'. $column[$j].'<br>';
+        endif;
+        if ($column[$j]['name'] == 'post_content'):
+            echo 'Post Content:'. $column[$j].'<br>';
+        endif;
+        if ($column[$j]['name'] == 'post_status'):
+            echo 'Post Status:'. $column[$j].'<br><br>';
+        endif;
+       
+    }
+}
