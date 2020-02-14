1c2f570469e6c04c048ff29b0774d99e5b326a25
AashishBarme <aashish.barme9@gmail.com>
2020-02-14 20:15:31 +0545

    functions.php added

--- config/database.php
+++ config/database.php
@@ -1,11 +1,14 @@
 <?php
-$servername = "localhost";
-$dbname = "wp_mysite";
-$dbuser = "admin";
-$dbpassword = "65403";
 
-$connection = mysqli_connect($servername,$dbuser,$dbpassword,$dbname);
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
+function dbConfig(){
+    $servername = "localhost";
+    $dbname = "wp_mysite";
+    $dbuser = "admin";
+    $dbpassword = "65403";
+
+    $connection = mysqli_connect($servername,$dbuser,$dbpassword,$dbname);
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    return $connection;
 }
-
--- index.php
+++ index.php
@@ -1,30 +1,22 @@
-<?php
-include 'config/database.php';
 
-$xml = simplexml_load_file('xml/td_posts.xml');
-$table = $xml->table;
 
-$post_date = date("Y-m-d h:i:sa");
-for ($i = 0 ; $i < count($table); $i++ )
-{
-    $column = $table[$i]->column;
-    for($j = 0; $j < count($column); $j++){
-        if ($column[$j]['name'] == 'post_title'):
-          $post_title = $column[$j];
-        endif;
-        if ($column[$j]['name'] == 'post_content'):
-            $post_content =  $column[$j];
-        endif;
-        if ($column[$j]['name'] == 'post_status'):
-            $post_status =  $column[$j];
-        endif;
-    }
-    
-    $query = "INSERT into wp_posts (post_title,post_content,post_status,post_type,post_date) values ('$post_title','$post_content','$post_status','post','$post_date')";
-    $run = mysqli_query($connection,$query);
-    if ($run){
-        echo "Post Insert";
-    } else {
-        echo "error";
-    }
-}
+<html>
+ <head>
+  <title>XML to WP Post Creator</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+ </head>
+ <body> 
+    <div class="container">
+      <div class="row">
+            <form action="output.php" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="xmlfile">Example file input</label>
+                <input type="file" name="xmldata" class="form-control-file" id="xmlfile">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </form>   
+        </div>
+    </div>         
+ </body>
+ </html>
